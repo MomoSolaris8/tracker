2eb37ebcaa6a33821cb6d4d638652a00ff2716a3
momo <momosunshine@gmail.com>
2023-04-06 22:51:29 +0200

    frist commit with mongodb atlas

---
+++ client/server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// MongoDB connection
+const uri = process.env.ATLAS_URI;
+//mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message))
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
+
+
+//app.listen(port, () => {
+  //  console.log(`Server is running on port: ${port}`);
+//});
